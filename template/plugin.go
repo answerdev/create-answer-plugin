8397dfbbe5a5107888d1b5c37031281056a65600
robin <robin@rnode.me>
2024-07-15 16:56:01 +0800

    chore: Create info.yaml file for plugin configuration

--- template/plugin.go
+++ template/plugin.go
@@ -1,17 +1,24 @@
 package answer
-import "github.com/apache/incubator-answer/plugin"
+import (
+  "github.com/apache/incubator-answer/plugin"
+
+  "github.com/apache/incubator-answer-plugins/util"
+)
 type {{plugin_display_name}} struct {
 }
 func init() {
 	plugin.Register(&{{plugin_display_name}}{})
 }
 func ({{plugin_display_name}}) Info() plugin.Info {
+  info := &util.Info{}
+	info.GetInfo()
+
   return plugin.Info{
     Name:        plugin.MakeTranslator("{{plugin_display_name}}"),
-    SlugName:    "{{plugin_slug_name}}",
+    SlugName:    info.SlugName,
     Description: plugin.MakeTranslator(""),
-    Author:      "",
-    Version:     "0.0.1",
-    Link:        "",
+    Author:      info.Author,
+    Version:     info.Version,
+    Link:        info.Link,
   }
 }
